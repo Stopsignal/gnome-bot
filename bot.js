7d7d836d7090073536ea46f0fadd42ce69d8e1be
Stopsignal <kettu1999@hotmail.com>
2017-12-12 06:31:46 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,23 +5,19 @@ client.on('ready', () => {
     console.log('listo!');
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
+client.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == 'gnomo, ') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
 	args = args.splice(1);
 	switch(cmd) {
-            // !ping
             case 'ping':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
             break;
-            // Just add any case commands if you want to..
          }
      }
 });
