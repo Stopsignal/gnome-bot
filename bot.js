e1fe84db9435ea7d6b644b4ceb4442d9506712b1
Stopsignal <kettu1999@hotmail.com>
2017-12-12 18:32:36 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,9 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+	if (message.content.toLowerCase() === 'gnomo, help' || message.content.toLowerCase() === 'gnomo, ayuda') {
+    		message.channel.send('que crees que soy, un bot?');
+  	} else
 	if (message.content.toLowerCase() === 'gnomo, hola' || message.content.toLowerCase() === 'gnomo, hola!' || message.content.toLowerCase() === 'hola gnomo' || message.content.toLowerCase() === 'hola gnomo!') {
 		var array = ["hola che!", "que tal!", "hooooola", "saludos!"];
     		message.channel.send(chooseFromArray(array));
@@ -13,8 +16,12 @@ client.on('message', message => {
     	if (message.content.toLowerCase() === 'gnomo, ping') {
     		message.channel.send('pong!');
   	} else
-	if (message.content.toLowerCase() === 'gnomo, putea a doritos') {
-    		message.channel.send('lpm doritos');
+	if (message.content.toLowerCase() === 'gnomo, putea a doritos' || message.content.toLowerCase() === 'gnomo, doritos' || message.content.toLowerCase() === 'gnomo, ya sabes que hacer') {
+		var array = ["lpm doritos", "uff doritos la concha de la lora", "doritos lpm", "uff basta doritos"];
+    		message.channel.send(chooseFromArray(array));
+  	} else 
+	if (message.content.toLowerCase() === 'gnomo, penis') {
+    		message.channel.send('you will get that at night');
   	}
 	
 });
