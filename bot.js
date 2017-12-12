237f3898974d3f01d727826402c79d672dc94f4d
Stopsignal <kettu1999@hotmail.com>
2017-12-12 05:31:47 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,28 +5,10 @@ client.on('ready', () {
     console.log('listo!');
 });
 
-client.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == '//') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-		case 'saludo':
-			bot.sendMessage({
-				to: channelID, 
-				message: 'que tal '+ user.username
-			});
-		break;
-         }
-     }
+client.on('message', message => {
+	if (message.content === 'ping') {
+		message.reply('pong');
+	}
 });
-
+	
 client.login(process.env.BOT_TOKEN);
