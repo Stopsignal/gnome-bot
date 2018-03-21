f80df49bf49cffe8bc9cc164cfeeadcb0e386e0a
Stopsignal <kettu1999@hotmail.com>
2018-03-21 18:43:50 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ client.on('message', message => {
 	if (message.content.toLowerCase().substring(0, 11) == 'gnomo, deci') {
 		//let myRole = message.guild.roles.find("name", "Stop!");
 		if (message.member.roles.find("name", "Stop!")){
-		    	s = message.content.toLowerCase().substring(13, message.content.length);
+		    	s = message.content.toLowerCase().substring(12, message.content.length);
 			message.channel.send(s);
 		} else {
 		       message.channel.send('?');
